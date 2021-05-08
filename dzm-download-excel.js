3992b067146ac3f8345cec0cf3fd9e2b8433604e
dengzemiao <dengzemiaovip@163.com>
2021-05-08 16:54:51 +0800

    支持日期格式显示

--- dzm-download-excel.js
+++ dzm-download-excel.js
@@ -17,6 +17,7 @@
  * @param {*} fileSuffix 文件后缀（选填，默认 xls，(目前仅支持 xls，xlsx))
  */
  function EXDownloadManager (sheets, columns, beforeChange, fileName, fileSuffix) {
+
   // 检查数据
   if (!sheets || !sheets.length || !columns || !columns.length) { return }
 
@@ -53,7 +54,7 @@
         // 单元格数据
         var itemData = {
           data: columnData,
-          dataType: undefined
+          dataType: column.dataType
         }
         // 准备将数据加入 Row 中
         if (beforeChange) { itemData = beforeChange(itemData, column.field) }
@@ -102,7 +103,7 @@ function EXDownloadChildren (rows, columns, children, beforeChange) {
         // 单元格数据
         var itemData = {
           data: columnData,
-          dataType: undefined
+          dataType: column.dataType
         }
         // 准备将数据加入 Row 中
         if (beforeChange) { itemData = beforeChange(itemData, column.field) }
@@ -164,9 +165,12 @@ function GetColumnData(columnField, rowData) {
           {
             // 展示数据
             data: 123,
-            // 数据类型，首字母大写 (可选值，可不传，可为空，默认会使用 data 的数据类型)
-            // dataType：Number、String、Boolean、DateTime、Error ...
-            dataType: 'Number'  // Number 类型长度最大只能 11 位数字，超过会自动转换为 String 存储
+            // 数据类型，首字母大写 (可选值，默认为空，默认会使用 data 的原始类型)
+            // dataType：Number、String、Boolean、Date ...
+            // Number：类型长度最大只能 11 位数字，超过会自动转换为 String 存储
+            dataType: 'Number', 
+            // （暂未开放，先配置 dataType='Date' 使用）样式，默认只支持配置将 日期字符串转 换成 Excel日期格式
+            style: {}
           }
         ]
       ]
@@ -229,59 +233,112 @@ function EXDownload (sheets, fileName, fileSuffix) {
   xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet"
   xmlns:html="http://www.w3.org/TR/REC-html40">`
 
+  // Styles 数组
+  var EXStyleStrings = []
+
+  // Sheets 数组
+  var EXSheetStrings = []
+
   // 便利 Worksheet
-  EXSheets.forEach((sheet, index) => {
-    // 拼接名称
-    if (!fileName) { EXFileName += `${sheet.name}${index ? '-' : ''}` }
+  EXSheets.forEach((sheet, sheetIndex) => {
+    // 没有指定文件名称，则组装一个文件名称
+    if (!fileName) { EXFileName += `${sheet.name}${sheetIndex ? '-' : ''}` }
 
     // Worksheet 头部
-    EXString += `<Worksheet ss:Name="${sheet.name}">`
+    var EXSheetString = `<Worksheet ss:Name="${sheet.name}">`
 
     // Table 头部
-    EXString += '<Table>'
+    EXSheetString += '<Table>'
 
     // 便利 Row
-    sheet.rows.forEach((row) => {
+    sheet.rows.forEach((row, rowIndex) => {
       // Row 头部
-      EXString += '<Row>'
+      EXSheetString += '<Row>'
 
       // 便利 Cell
-      row.forEach((cell) => {
+      row.forEach((cell, cellIndex) => {
+        // 组合 StyleID
+        var styleID = `s${sheetIndex}${rowIndex}${cellIndex}`
+
         // 获取数据类型
         var dataType = cell.dataType || typeof (cell.data)
 
-        // 类型首字母大写
-        dataType = dataType.replace(dataType[0], dataType[0].toUpperCase())
+        // 检查是否存在数据类型，不存在则不需要管
+        if (dataType !== 'undefined') {
+          // 类型首字母大写
+          dataType = dataType.replace(dataType[0], dataType[0].toUpperCase())
+        }
 
         // 超过 11 位的数字需要转成字符串
         if (dataType === 'Number' && cell.data > 10000000000) { dataType = 'String' }
 
-        // Cell 头部
-        EXString += '<Cell>'
+        // Date 类型处理
+        if (cell.dataType === 'Date') {
 
-        // Data 头部
-        EXString += `<Data ss:Type="${dataType}">`
+          // 没有数据，格式强行转换为 String 格式
+          dataType = 'String'
 
-        // Data 数据
-        EXString += `${cell.data}`
+          // 有数据
+          if (cell.data) {
 
-        // Data 尾部
-        EXString += '</Data>'
+            // 格式强行转换为 Number 格式
+            dataType = 'Number'
 
-        // Cell 尾部
-        EXString += '</Cell>'
+            // 临时设置一个指定值，用于测试
+            cell.data = EXDateNumber(cell.data)
+
+            //  获取 Style
+            var EXStyleString = EXStyleDate(styleID)
+
+            // 添加到 Styles 列表
+            EXStyleStrings.push(EXStyleString)
+          }
+        }
+
+        // 获取 Cell 单元格
+        var EXCellString = EXCell(cell.data, dataType, styleID)
+
+        // 拼接 Cell 单元格
+        EXSheetString += EXCellString
+
       }) // 便利 Cell
 
       // Row 尾部
-      EXString += '</Row>'
+      EXSheetString += '</Row>'
     }) // 便利 Cell
 
     // Table 尾部
-    EXString += '</Table>'
+    EXSheetString += '</Table>'
 
     // Worksheet 尾部
-    EXString += '</Worksheet>'
+    EXSheetString += '</Worksheet>'
+
+    // 添加到 Sheets 列表
+    EXSheetStrings.push(EXSheetString)
+
   }) // 便利 Worksheet
+
+  // Styles 是否有值
+  if (EXStyleStrings.length) {
+    // Styles 头部
+    EXString += `<Styles>`
+
+    // EXStyleStrings 添加到 Workbook
+    EXStyleStrings.forEach((EXStyleString) => {
+      // 拼接
+      EXString += EXStyleString
+    })
+
+    // Styles 头部
+    EXString += `</Styles>`
+
+  }
+
+  // EXSheetStrings 添加到 Workbook
+  EXSheetStrings.forEach((EXSheetString) => {
+    // 拼接
+    EXString += EXSheetString
+  })
 
   // Workbook 尾部
   EXString += '</Workbook>'
@@ -300,6 +357,55 @@ function EXDownload (sheets, fileName, fileSuffix) {
   alink.click()
   // 移除 a 标签
   // document.body.removeChild(alink)
+}
+
+// 获取 Style - Short Date 日期格式 YYYY/M/D
+function EXStyleDate (styleID) {
+  // Style 头部
+  var EXStyleString = `<Style ss:ID="${styleID}">`
+  // Style 内容
+  EXStyleString += `<NumberFormat ss:Format="Short Date"/>`
+  // Style 尾部
+  EXStyleString += `</Style>`
+  // 返回
+  return EXStyleString
+}
+
+// 获取 Cell 单元格
+function EXCell (data, dataType, styleID) {
+  // Cell 头部
+  var EXCellString = `<Cell ss:StyleID="${styleID}">`
+  // Data 头部
+  EXCellString += `<Data ss:Type="${dataType || ''}">`
+  // Data 数据
+  EXCellString += `${data || ''}`
+  // Data 尾部
+  EXCellString += '</Data>'
+  // Cell 尾部
+  EXCellString += '</Cell>'
+  // 返回
+  return EXCellString
+}
+
+// 获取 日期天数
+function EXDateNumber (data) {
+  // 是否有数据
+  if (data) {
+    // 开始时间
+    let startTime = new Date('1900/01/01')
+    // 结束时间
+    var endTimeString = data.replaceAll('-', '/')
+    endTimeString = endTimeString.replaceAll('~', '/')
+    endTimeString = endTimeString.replaceAll('年', '/')
+    endTimeString = endTimeString.replaceAll('月', '/')
+    endTimeString = endTimeString.replaceAll('日', '')
+    let endTime = new Date(endTimeString)
+    // 间隔天数，为什么需要 +2，这里计算出来的只是中间的差值天数，加开头结尾的各一天就是2天，所以 +2
+    return Math.floor((endTime - startTime) / 1000 / 60 / 60 / 24) + 2
+  } else {
+    // 返回空
+    return ''
+  }
 }
 
 // 导出
